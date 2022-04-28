9bf0af16b191ca1abe837f3915206e971cca27ca
Yitian Yan <yitianya@usc.edu>
2022-04-28 11:41:40 -0700

    add Constant.java

---
+++ Constant.java
@@ -0,0 +1,16 @@
+package mypackage;
+
+import java.util.regex.Pattern;
+
+public class Constant {
+    //TODO replace it with your DB credentials
+	static public String url = "jdbc:mysql://localhost:3306/final_project?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+    static public String DBUserName = "root";
+    static public String DBPassword = "root";
+    static public Pattern namePattern = Pattern.compile("^[ A-Za-z]+$");
+    static public Pattern emailPattern = Pattern.compile("^[a-zA-Z0-9_+&*-]+(?:\\."
+            + "[a-zA-Z0-9_+&*-]+)*@"
+            + "(?:[a-zA-Z0-9-]+\\.)+[a-z"
+            + "A-Z]{2,7}$");
+
+}
